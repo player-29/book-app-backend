886b1d9a992a071be4cb611041caab0d6a8e276d
ayushrathour <ayushrathour2910@gmail.com>
2024-12-25 21:18:19 +0530

    changes in syntax

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const cors=require("cors");
-const port = process.env.PORT || 5000;
+
 const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 //middleware
@@ -13,10 +14,11 @@ app.use(cors({
 }))
 
 // routes
-const bookRoutes= require('./src/books/book.route');
-const orderRoutes= require('./src/orders/order.route');
-const userRoutes = require('./src/users/user.route');
-const adminRoutes = require('./src/stats/admin.stats');
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require("./src/orders/order.route")
+const userRoutes =  require("./src/users/user.route")
+const adminRoutes = require("./src/stats/admin.stats")
+
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes);
@@ -28,8 +30,8 @@ app.use("/api/admin", adminRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use('/', (req, res) => {
-    res.send('Book store server is running!')
+  app.use("/", (req, res) => {
+    res.send('Book store server is running!');
   });
 
 }
